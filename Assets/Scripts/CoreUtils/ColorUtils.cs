b9bd97077c23ae875a6c40b5d83f629a708d5606
kemarine <kemarine@korea.ac.kr>
2024-07-05 22:09:29 +0900

    mod ColorUtils - change spec of RGB CMYK trans func

--- Assets/Scripts/CoreUtils/ColorUtils.cs
+++ Assets/Scripts/CoreUtils/ColorUtils.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorUtils : MonoBehaviour
+public class ColorUtils
 {
     // Get Random Color (Without Candidate)
     public static Color GetRandomColor()
@@ -33,8 +33,12 @@ public class ColorUtils : MonoBehaviour
         return new Color(r, g, b);
     }
 
-    public static void RGBtoCMYK(float r, float g, float b, out float c, out float m, out float y, out float k)
+    public static void RGBtoCMYK(Color color, out float c, out float m, out float y, out float k)
     {
+        float r = color.r;
+        float g = color.g;
+        float b = color.b;
+
         float cyan = 1f - r;
         float magenta = 1f - g;
         float yellow = 1f - b;
@@ -57,29 +61,28 @@ public class ColorUtils : MonoBehaviour
         }
     }
 
-    public static void CMYKtoRGB(float c, float m, float y, float k, out float r, out float g, out float b)
+    public static Color CMYKtoRGB(float c, float m, float y, float k)
     {
-        r = Mathf.Clamp01(1f - Mathf.Min(1f, c * (1f - k) + k));
-        g = Mathf.Clamp01(1f - Mathf.Min(1f, m * (1f - k) + k));
-        b = Mathf.Clamp01(1f - Mathf.Min(1f, y * (1f - k) + k));
+        float r = Mathf.Clamp01(1f - Mathf.Min(1f, c * (1f - k) + k));
+        float g = Mathf.Clamp01(1f - Mathf.Min(1f, m * (1f - k) + k));
+        float b = Mathf.Clamp01(1f - Mathf.Min(1f, y * (1f - k) + k));
+
+        return new Color(r, g, b);
     }
 
     // Mix Two Colors (Subtractive Mixing)
     public static Color MixColors(Color color1, Color color2)
     {
         float c1 = 0f, m1 = 0f, y1 = 0f, k1 = 0f, c2 = 0f, m2 = 0f, y2 = 0f, k2 = 0f;
-        RGBtoCMYK(color1.r, color1.g, color1.b, out c1, out m1, out y1, out k1);
-        RGBtoCMYK(color2.r, color2.g, color2.b, out c2, out m2, out y2, out k2);
+        RGBtoCMYK(color1, out c1, out m1, out y1, out k1);
+        RGBtoCMYK(color2, out c2, out m2, out y2, out k2);
         
         float c = Mathf.Clamp01(c1 + c2);
         float m = Mathf.Clamp01(m1 + m2);
         float y = Mathf.Clamp01(y1 + y2);
         float k = Mathf.Clamp01(k1 + k2);
 
-        float r = 0f, g = 0f, b = 0f;
-        CMYKtoRGB(c, m, y, k, out r, out g, out b);
-
-        return new Color(r, g, b);
+        return CMYKtoRGB(c, m, y, k);
     }
 
     // Todo : Return Color List w.Gradation
