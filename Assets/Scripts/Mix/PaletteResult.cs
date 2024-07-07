68d9dcc6dcb6cb53e1cdee0363de32c731b0d0c8
hoohoohoo29 <nkhoo0209@gmail.com>
2024-07-07 18:35:25 +0900

    mod palette

--- Assets/Scripts/Mix/PaletteResult.cs
+++ Assets/Scripts/Mix/PaletteResult.cs
@@ -7,29 +7,24 @@ using UnityEngine.UI;
 
 public class PaletteResult : MonoBehaviour
 {
-    public GameObject ob1, ob2, ob3;
-    public Image image;
-    public int AClicked;
-    public int BClicked;
-    public int CClicked;
+    public Button btn1, btn2, btn3, btnreset;
+    public Image resultimage;
+    public int AClicked=0, BClicked=0, CClicked=0;
     void Start()
     {
-        AClicked = ob1.GetComponent<PaletteButtonClick>().ButtonClickedTimes;
-        BClicked = ob2.GetComponent<PaletteButtonClick>().ButtonClickedTimes;
-        CClicked = ob3.GetComponent<PaletteButtonClick>().ButtonClickedTimes;
-        image = GetComponent<Image>();
+        resultimage = GetComponent<Image>();
+        btn1.onClick.AddListener(() => { if (AClicked < 5) { AClicked++; } });
+        btn2.onClick.AddListener(() => { if (BClicked < 5) { BClicked++; } });
+        btn3.onClick.AddListener(() => { if (CClicked < 5) { CClicked++; } });
+        btnreset.onClick.AddListener(() => { AClicked=0;BClicked = 0;CClicked = 0; });
     }
 
-    // Update is called once per frame
     void Update()
     {
-        AClicked = ob1.GetComponent<PaletteButtonClick>().ButtonClickedTimes;
-        BClicked = ob2.GetComponent<PaletteButtonClick>().ButtonClickedTimes;
-        CClicked = ob3.GetComponent<PaletteButtonClick>().ButtonClickedTimes;
-        Color colorR = new Color(255f*AClicked / (255 * 4), 0f, 0f);
-        Color colorG = new Color(0f, 255f*BClicked / (255 * 4), 0f);
-        Color colorB = new Color(0f, 0f, 255f*CClicked / (255 * 4));
+        Color colorR = new Color(255f*AClicked / (255 * 5), 0f, 0f);
+        Color colorG = new Color(0f, 255f*BClicked / (255 * 5), 0f);
+        Color colorB = new Color(0f, 0f, 255f*CClicked / (255 * 5));
         Color tempColor = ColorUtils.MixLights(colorR,ColorUtils.MixLights(colorG,colorB));
-        image.color = tempColor;
+        resultimage.color = tempColor;
     }
 }
