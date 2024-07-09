fba4648295a9f81ec0822c5f9dba0078764b537c
hoohoohoo29 <nkhoo0209@gmail.com>
2024-07-09 23:43:45 +0900

    mod palette
    add substractive mixing

--- Assets/Scripts/Mix/PaletteResult.cs
+++ Assets/Scripts/Mix/PaletteResult.cs
@@ -8,23 +8,40 @@ using UnityEngine.UI;
 public class PaletteResult : MonoBehaviour
 {
     public Button btn1, btn2, btn3, btnreset;
-    public Image resultimage;
-    public int AClicked=0, BClicked=0, CClicked=0;
+    public Image resultimage,btn1image,btn2image,btn3image;
+    public int AClicked=0, BClicked=0, CClicked=0, MixType=1;
+    public Color btncolorA, btncolorB, btncolorC, colorA,colorB,colorC;
     void Start()
     {
         resultimage = GetComponent<Image>();
-        btn1.onClick.AddListener(() => { if (AClicked < 5) { AClicked++; } });
-        btn2.onClick.AddListener(() => { if (BClicked < 5) { BClicked++; } });
-        btn3.onClick.AddListener(() => { if (CClicked < 5) { CClicked++; } });
+        btncolorA = btn1image.color;
+        btncolorB = btn2image.color;
+        btncolorC = btn3image.color;
+        colorA = btncolorA;
+        colorB = btncolorB;
+        colorC = btncolorC;
+        btn1.onClick.AddListener(() => { if (AClicked < 4) { AClicked++; } });
+        btn2.onClick.AddListener(() => { if (BClicked < 4) { BClicked++; } });
+        btn3.onClick.AddListener(() => { if (CClicked < 4) { CClicked++; } });
         btnreset.onClick.AddListener(() => { AClicked=0;BClicked = 0;CClicked = 0; });
     }
 
     void Update()
     {
-        Color colorR = new Color(255f*AClicked / (255 * 5), 0f, 0f);
-        Color colorG = new Color(0f, 255f*BClicked / (255 * 5), 0f);
-        Color colorB = new Color(0f, 0f, 255f*CClicked / (255 * 5));
-        Color tempColor = ColorUtils.MixLights(colorR,ColorUtils.MixLights(colorG,colorB));
-        resultimage.color = tempColor;
+       
+        if (MixType == 0) //Additive Mixing
+        {
+            colorA = btncolorA * (AClicked / 4f);
+            colorB = btncolorB * (BClicked / 4f);
+            colorC = btncolorC * (CClicked / 4f);
+            resultimage.color = ColorUtils.MixLights(colorA, ColorUtils.MixLights(colorB, colorC));
+        }
+        else //Subtractive Mixing
+        {
+            colorA = Color.white - (Color.white - btncolorA) * AClicked / 4f;
+            colorB = Color.white - (Color.white - btncolorB) * BClicked / 4f;
+            colorC = Color.white - (Color.white - btncolorC) * CClicked / 4f;
+            resultimage.color = ColorUtils.MixColors(colorA, ColorUtils.MixColors(colorB, colorC));
+        }
     }
 }
