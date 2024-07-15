5ebf6e983cca636866d0293aec4c13cea2b0c689
hoohoohoo29 <nkhoo0209@gmail.com>
2024-07-15 21:15:40 +0900

    mod palette

--- Assets/Scripts/Mix/PaletteResult.cs
+++ Assets/Scripts/Mix/PaletteResult.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class PaletteResult : MonoBehaviour
@@ -16,28 +17,64 @@ public class PaletteResult : MonoBehaviour
         colorA = btn1Image.color;
         colorB = btn2Image.color;
         colorC = btn3Image.color;
-        btn1.onClick.AddListener(() => { if (AClick < 5) { AClick++; } });
-        btn2.onClick.AddListener(() => { if (BClick < 5) { BClick++; } });
-        btn3.onClick.AddListener(() => { if (CClick < 5) { CClick++; } });
-        btnReset.onClick.AddListener(() => { AClick=0;BClick = 0;CClick = 0; });
-    }
-
-    void Update()
-    {
-       
-        if (mixType == 0) //Additive Mixing
-        {
-            colorA = btn1Image.color * (AClick / 5f);
-            colorB = btn2Image.color * (BClick / 5f);
-            colorC = btn3Image.color * (CClick / 5f);
-            resultImage.color = ColorUtils.MixLights(colorA, ColorUtils.MixLights(colorB, colorC));
-        }
-        else //Subtractive Mixing
-        {
-            colorA = Color.white - (Color.white - btn1Image.color) * AClick / 5f;
-            colorB = Color.white - (Color.white - btn2Image.color) * BClick / 5f;
-            colorC = Color.white - (Color.white - btn3Image.color) * CClick / 5f;
-            resultImage.color = ColorUtils.MixColors(colorA, ColorUtils.MixColors(colorB, colorC));
-        }
+        btn1.onClick.AddListener(() => { 
+            if (AClick < 5) { AClick++; }
+            if (mixType == 0) //Additive Mixing
+            {
+                colorA = btn1Image.color * (AClick / 5f);
+                colorB = btn2Image.color * (BClick / 5f);
+                colorC = btn3Image.color * (CClick / 5f);
+                resultImage.color = ColorUtils.MixLights(colorA, ColorUtils.MixLights(colorB, colorC));
+            }
+            else //Subtractive Mixing
+            {
+                colorA = Color.white - (Color.white - btn1Image.color) * AClick / 5f;
+                colorB = Color.white - (Color.white - btn2Image.color) * BClick / 5f;
+                colorC = Color.white - (Color.white - btn3Image.color) * CClick / 5f;
+                resultImage.color = ColorUtils.MixColors(colorA, ColorUtils.MixColors(colorB, colorC));
+            }
+        });
+        btn2.onClick.AddListener(() => { if (BClick < 5) { BClick++; }
+            if (mixType == 0) //Additive Mixing
+            {
+                colorA = btn1Image.color * (AClick / 5f);
+                colorB = btn2Image.color * (BClick / 5f);
+                colorC = btn3Image.color * (CClick / 5f);
+                resultImage.color = ColorUtils.MixLights(colorA, ColorUtils.MixLights(colorB, colorC));
+            }
+            else //Subtractive Mixing
+            {
+                colorA = Color.white - (Color.white - btn1Image.color) * AClick / 5f;
+                colorB = Color.white - (Color.white - btn2Image.color) * BClick / 5f;
+                colorC = Color.white - (Color.white - btn3Image.color) * CClick / 5f;
+                resultImage.color = ColorUtils.MixColors(colorA, ColorUtils.MixColors(colorB, colorC));
+            }
+        });
+        btn3.onClick.AddListener(() => { if (CClick < 5) { CClick++; }
+            if (mixType == 0) //Additive Mixing
+            {
+                colorA = btn1Image.color * (AClick / 5f);
+                colorB = btn2Image.color * (BClick / 5f);
+                colorC = btn3Image.color * (CClick / 5f);
+                resultImage.color = ColorUtils.MixLights(colorA, ColorUtils.MixLights(colorB, colorC));
+            }
+            else //Subtractive Mixing
+            {
+                colorA = Color.white - (Color.white - btn1Image.color) * AClick / 5f;
+                colorB = Color.white - (Color.white - btn2Image.color) * BClick / 5f;
+                colorC = Color.white - (Color.white - btn3Image.color) * CClick / 5f;
+                resultImage.color = ColorUtils.MixColors(colorA, ColorUtils.MixColors(colorB, colorC));
+            }
+        });
+        btnReset.onClick.AddListener(() => { AClick=0;BClick = 0;CClick = 0; 
+            if (mixType == 0) //Additive Mixing
+            {
+                resultImage.color = Color.black;
+            }
+            else //Subtractive Mixing
+            {
+                resultImage.color = Color.white;
+            }
+        });
     }
 }
