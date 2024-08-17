ffff48798787c6b38dfd099898de29a9932c7d38
tmdghks <tmdghks12@korea.ac.kr>
2024-08-17 17:14:28 +0900

    add: Tanmak.Patterns namespace

--- Assets/Scripts/MiniGame/Tanmak/BulletPatternManager/BulletPatterns/CircularPattern.cs
+++ Assets/Scripts/MiniGame/Tanmak/BulletPatternManager/BulletPatterns/CircularPattern.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Tanmak
+namespace Tanmak.BulletPatterns
 {
     public class CircularPattern : BulletPatternBase
     {
